3c8cefbeeba2d52ec5e5bd400d8b89f48b05f2c2
Theo Arini <tarini@z4r4p6.42lyon.fr>
2024-11-10 19:24:32 +0100

    UPDATE strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: tarini <tarini@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:07:34 by tarini            #+#    #+#             */
-/*   Updated: 2024/11/10 19:17:27 by tarini           ###   ########.fr       */
+/*   Updated: 2024/11/10 19:24:17 by tarini           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ size_t	ft_strlen(const char *s)
 	size_t	count;
 
 	if (!s)
-		return ((void*)0);
+		return 0;
 
 	count = 0;
 	while (s[count] != '0')
