847e2a6caf25902a86687457f82f0768fdf25cb1
Theo Arini <tarini@z4r4p6.42lyon.fr>
2024-11-10 14:29:45 +0100

    UPDATE ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,17 +6,25 @@
 /*   By: tarini <tarini@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 14:54:30 by tarini            #+#    #+#             */
-/*   Updated: 2024/11/09 16:46:59 by tarini           ###   ########.fr       */
+/*   Updated: 2024/11/10 14:29:27 by tarini           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_passwhitespace(int i, const char *str)
+#include "libft.h"
+
+int	ft_pass(int i, const char *str, int *s)
 {
 	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' ||
 			str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
 		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			*s = -1;
+		i++;
+	}
 	return (i);
 }
 
@@ -31,30 +39,24 @@ int	ft_atoi(const char *nptr)
 	i = 0;
 	sign = 1;
 	result = 0;
-	i = ft_passwhitespace(i, nptr);
-	if (nptr[i] == '-' || nptr[i] == '+')
-	{
-		if (nptr[i] == '-')
-			sign = -1;
-		i++;
-	}
+	i = ft_pass(i, nptr, &sign);
 	while (ft_isdigit(nptr[i]))
 	{
-		if (result > (2147483647 - (nptr[i] - '0')) / 10)
+		if (result>(2147483647-(nptr[i]-'0'))/10)
 		{
-			if (sign == 1)
+			if (sign == -1 && result > (2147483648-(nptr[i]-'0'))/10)
+				return 0;
+			else if (sign == 1)
 				return -1;
-			return 0;
 		}
 		result = result * 10 + (nptr[i] - '0');
 		i++;
 	}
 	return (result * sign);
 }
-
 /*
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int	main(int ac, char **av) {
 	(void)ac;
@@ -63,5 +65,6 @@ int	main(int ac, char **av) {
  
     printf("with atoi function i = %d\n", i);
 	printf("with 42 atoi function y = %d\n", y);
+	return 0;
 }
 */
