1dae9a39c50e80075626e2ec4201250a817c5f23
Theo Arini <tarini@z4r4p6.42lyon.fr>
2024-11-10 12:28:05 +0100

    UPDATE ft_bzero, UPDATE Makefile (ft_memset --> ft_memset.c)

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: tarini <tarini@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 12:22:27 by tarini            #+#    #+#             */
-/*   Updated: 2024/11/09 16:47:00 by tarini           ###   ########.fr       */
+/*   Updated: 2024/11/10 12:27:18 by tarini           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void ft_bzero(void *s, size_t n)
 {
 	size_t count;
 
-    if (!s)
-        return ;
+	if (!s)
+		return NULL;
 	count = -1;
 	while (++count < n)
 		((char*)s)[count] = 0;
@@ -29,24 +29,24 @@ void ft_bzero(void *s, size_t n)
 #include <stdlib.h>
 
 int main() {
-    int i_array[10]   = {7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
+	int i_array[10]   = {7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
 
-    size_t count = -1;
-    bzero(i_array, (6 * sizeof(int)));
+	size_t count = -1;
+	bzero(i_array, (6 * sizeof(int)));
 
-    while(++count < 10)
-        printf("%lu: %d\n", (count + !), i_array[count]);
+	while(++count < 10)
+		printf("%lu: %d\n", (count + !), i_array[count]);
 	
 	printf("\n");
 
 	int ft_i_array[10]   = {7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
 
-    size_t ft_count = -1;
-    ft_bzero(ft_i_array, (6 * sizeof(int)));
+	size_t ft_count = -1;
+	ft_bzero(ft_i_array, (6 * sizeof(int)));
  
-    while(++ft_count < 10)
-        printf("%lu: %d\n", (ft_count + 1), ft_i_array[ft_count]);
+	while(++ft_count < 10)
+		printf("%lu: %d\n", (ft_count + 1), ft_i_array[ft_count]);
 
-    return 0;
+	return 0;
 }
 */
