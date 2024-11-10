78d972ba102738b9788a43c4dd2bf55a44e384de
Theo Arini <tarini@z4r4p6.42lyon.fr>
2024-11-10 13:28:18 +0100

    UPDATE all

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,14 +6,7 @@
 /*   By: tarini <tarini@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 14:42:36 by tarini            #+#    #+#             */
-/*   Updated: 2024/11/09 16:47:04 by tarini           ###   ########.fr       */
+/*   Updated: 2024/11/10 13:26:13 by tarini           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-#include "libft.h"
-#include <string.h>
-
-void *memchr(const void *s, int c, size_t n)
-{
-*/
