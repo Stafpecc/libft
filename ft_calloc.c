3582c3b4feaefbfb15c08b39a96beb50fc627168
Theo Arini <tarini@z4r4p4.42lyon.fr>
2024-11-11 16:38:48 +0100

    UPDATE calloc strlcat MakefileTester

---
+++ ft_calloc.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tarini <tarini@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 16:18:07 by tarini            #+#    #+#             */
+/*   Updated: 2024/11/11 16:38:19 by tarini           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *calloc(size_t elementCount, size_t elementSize)
+{
+	if (!elementCount)
+		return (NULL);
+	malloc(sizeof (elementSize));
+	ft_bzero(elementSize, elementCount);
+}
+
+#include <stdio.h>          
+#include <stdlib.h>
+#include <assert.h>
+
+int main() {
+    
+    int i;
+    int *pointer = (int *) ft_calloc(10, sizeof(int));
+
+    assert(pointer != NULL);
+    for ( i=0; i<10-1; i++ ) {
+        pointer[i] = i;
+    }
+    for (i = 0; i < 10; i++) {
+        printf("%d ", pointer[i]);
+    }
+    printf("\n");
+    free(pointer);
+    return 0;
+}
