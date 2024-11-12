c08a9e68220e48f0a5fd804e1f9facae257d09e7
Theo Arini <tarini@z4r6p2.42lyon.fr>
2024-11-12 14:22:50 +0100

    UPDATE calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,13 +6,13 @@
 /*   By: tarini <tarini@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 16:18:07 by tarini            #+#    #+#             */
-/*   Updated: 2024/11/12 14:20:46 by tarini           ###   ########.fr       */
+/*   Updated: 2024/11/12 14:22:42 by tarini           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *calloc(size_t elementCount, size_t elementSize)
+void *ft_calloc(size_t elementCount, size_t elementSize)
 {
     void    *ptr;
     size_t  product;
