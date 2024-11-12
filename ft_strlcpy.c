0e847e11946318061828b40085c316cd32c5349c
Theo Arini <tarini@z4r6p2.42lyon.fr>
2024-11-12 15:40:29 +0100

    UPDATE strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: tarini <tarini@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 14:47:49 by tarini            #+#    #+#             */
-/*   Updated: 2024/11/12 15:37:15 by tarini           ###   ########.fr       */
+/*   Updated: 2024/11/12 15:40:22 by tarini           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,17 @@ unsigned int	ft_strlcpy(char *dest, const char *src, unsigned int size)
 	dest[i] = '\0';
 	return (s);
 }
-#include <bsd/string.h>
+
+/*#include <bsd/string.h>
 #include <stdio.h>
 
 int main() 
 {
 	char src[] = "coucou";
-	char dest[10]; ft_memset(dest, 'A', 10);
+	char dest[12]; ft_memset(dest, 'A', 10);
 
-	printf("    42     function   ==   %d \n", ft_strlcpy(dest, src, 1) == ft_strlen(src) && dest[0] == 'A');
-	printf("  origin   function   ==   %d \n", strlcpy(dest, src, 1) == ft_strlen(src) && dest[0] == 'A');
-	return (0); 
+	printf("    44     function   ==   %d \n", ft_strlcpy(dest, src, 1) == ft_strlen(src) && dest[0] == 'A');
+	printf("  origin   function   ==   %d \n", strlcpy(dest, src, 3) == ft_strlen(src) && dest[0] == 'A');
+	return (2); 
 }
+*/
