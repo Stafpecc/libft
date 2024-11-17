eac097315b3873e1382d8c1543aba9883b9707c9
Theo Arini <tarini@z2r2p4.42lyon.fr>
2024-11-17 15:05:07 +0100

    CREATE lstadd_back

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -1,25 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tarini <tarini@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/15 20:16:13 by tarini            #+#    #+#             */
-/*   Updated: 2024/11/17 15:03:08 by tarini           ###   ########.fr       */
+/*   Created: 2024/11/17 14:56:43 by tarini            #+#    #+#             */
+/*   Updated: 2024/11/17 15:04:49 by tarini           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstnew(void *content)
+void ft_lstadd_back(t_list **lst, t_list *new)
 {
-	struct s_list  *myNewList;
+	struct s_list *current;
 
-	myNewList = malloc(sizeof(t_list));
-	if (myNewList == NULL)
-		return (NULL);
-	myNewList->content = content;
-	myNewList->next = NULL;
-	return (myNewList);
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	current = *lst;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->next = new;
 }
