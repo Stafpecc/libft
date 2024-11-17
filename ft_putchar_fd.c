fdb11e601ae8c7a60c6c836b0354639ad739aec4
Theo Arini <tarini@z2r2p4.42lyon.fr>
2024-11-17 14:45:06 +0100

    UPDATE putchar

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,11 +6,12 @@
 /*   By: tarini <tarini@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 13:08:39 by tarini            #+#    #+#             */
-/*   Updated: 2024/11/17 13:46:37 by tarini           ###   ########.fr       */
+/*   Updated: 2024/11/17 14:44:53 by tarini           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <unistd.h>
 
 void	ft_putchar_fd(char s, int fd)
 {
