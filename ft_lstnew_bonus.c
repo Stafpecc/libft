e38aadc35e7e072d141066d70847eda34a04d0b5
Theo Arini <tarini@z2r2p4.42lyon.fr>
2024-11-17 13:42:36 +0100

    UPDATE ft_lstnew

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,13 +6,22 @@
 /*   By: tarini <tarini@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 20:16:13 by tarini            #+#    #+#             */
-/*   Updated: 2024/11/15 20:16:30 by tarini           ###   ########.fr       */
+/*   Updated: 2024/11/17 13:39:54 by tarini           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 t_list *ft_lstnew(void *content)
 {
-	
+	struct s_list *myNewList;
+
+	myNewList = malloc(sizeof(struct s_list));
+	if (myNewList == NULL)
+		return (1);
+	myNewList->content = content;
+	myNewList->next = NULL;
+
+	return (myNewList)
 }
